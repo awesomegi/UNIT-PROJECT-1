1e77ae347c42c1230dbc7f9dc9e506f5d98eefe1
ghadi <ghadi.zareb@gmail.com>
2026-03-08 23:21:59 +0300

    Add fetch questions function in api.py

---
+++ modules/api.py
@@ -0,0 +1,10 @@
+
+
+'''
+This is your data source. Before writing it, ask yourself:
+
+How do I call a URL in Python? → requests.get(url)
+What do I get back? → a Response object, call .json() on it
+What if the internet is down? → try/except around the call
+What should the function return? → a clean list of question dicts
+'''
